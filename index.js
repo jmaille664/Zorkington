d274abd1804084ec65b427193156a64746abe3a6
jmaille664 <juliamaille@gmail.com>
2022-02-19 09:22:27 -0500

    :

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*-------------Boiler Plate ------------ */
+
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
@@ -7,14 +9,88 @@ function ask(questionText) {
   });
 }
 
-start();
+welcome();
 
-async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+async function welcome() {
+  const welcomeMessage = `The tardis(Time and Relative Dimensions in Space) lands on a new world. \n 
+Everything on the world is dead except there is a large metal city beyond a jungle. \n
+Would you like to explore this new world? [Yes] or [No] \n  >_`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
-  process.exit();
+  
+
+//Logic to check to see if the answer input is Yes or No
+while(true){
+  if (answer === "No"){
+    console.log("You remain in the Tardis and do not explore the new world :(")
+    process.exit()
+  } 
+  if (answer === "Yes"){
+  return startGame()
+  
+  //process.exit()
+  }
 }
+}
+
+//define your current location state
+//let currentLocation = Tardis;
+
+//Create a start game function to tell you what your current room is 
+async function startGame(){
+ console.log(`You are currently in the ${Tardis.description}`)
+}
+
+
+//uUtilizing the constructor method and passing in our arguments to construct
+class room {
+//mapping the 'this' keyword to the arguments passed in the constructor
+	constructor(name, connections, item){
+
+	this.name = name
+
+  this.connections = connections
+
+	this.item = item
+
+	}
+}
+
+
+//create new object of the room class - "Tardis"
+let Tardis = new room("Tardis",[]);
+
+//create new object of the room class - "Hall"
+let Hallway = new room("Tardis", "Labratory", "Cell");
+
+//create new object of the room class - "Power"
+let Power = new room();
+
+//Create new object of the room class -"Cell"
+let Cell = new room();
+
+//create new object of the room class - "Labratory"
+let Labratory = new room();
+
+//Create ne object of the room class - "Center"
+let Center = new room();
+
+//create lookup table to map the room keyword to the object
+let roomLookUp = {
+  tardis: Tardis,
+  hallway: Hallway,
+  power: Power,
+  cell: Cell,
+  labratory: Labratory,
+  center: Center,
+};
+
+//create state machine to hold allowable transitions
+let roomStateMachine = {
+  tardis: ["Hallway"],
+  hallway: ["Labratory", "Center", "Power"],
+  power: ["Hallway", "Cell"],
+  labratory:["Hallway", "Center"],
+  cell: ["Hallway"],
+  center: ["Hallway"],
+};
+
