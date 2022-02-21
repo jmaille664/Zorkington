4b34a658ffb4a1c621cbf4f8eaf4c9e9c681b9ca
jmaille664 <juliamaille@gmail.com>
2022-02-20 22:12:09 -0500

    Added functions to move around locations

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function ask(questionText) {
   });
 }
 
+//This is the introductory welcome message for the game. 
 welcome();
 
 async function welcome() {
@@ -32,31 +33,51 @@ while(true){
 //Creating the room class
 class room {
 //utilizing the constructor method and passing in arguments to constructor
-	constructor(name, description, item, locked){
+	constructor(name, description, inventory, locked){
 
   //mapping the 'this' keyword to the arguments passed in the constructor
 	this.name = name;
 
   this.description = description;
 
-	this.item = item;
+	this.inventory = inventory;
 
   this.locked = locked;
-
 	}
+
+  use() {
+    console.log("use");
+  }
+  
+  move(){
+    console.log("move");
+  }
+  talk(){
+    console.log("The people in the village are called the thal's.\n. The Thal's warn you that the city is inhabited by an evil alien race called the Daleks");
+  }
+
 }
 
+//Creating item class
+class item {
+  constructor(name, description){
+    this.name = name;
+    this.description = description;
+  }
+} 
+
+
 //create new object of the room class - "Tardis"
-let Tardis = new room("Tardis","Tardis");
+let Tardis = new room("Tardis","it is bigger on the inside thant it looks from the outside.");
 
-//create new object of the room class - "Hall"
-let Hallway = new room("Hallway", "Labratory", "Cell");
+//create new object of the room class - "Jungle"
+let Jungle = new room("Jungle", "it is desolate and nothing is growing.");
 
-//create new object of the room class - "Power"
-let Power = new room();
+//Create new object of the room class -"City"
+let City = new room();
 
-//Create new object of the room class -"Cell"
-let Cell = new room();
+//create new object of the room class - "Hallway"
+let Hallway = new room();
 
 //create new object of the room class - "Labratory"
 let Labratory = new room();
@@ -64,48 +85,108 @@ let Labratory = new room();
 //Create ne object of the room class - "Center"
 let Center = new room();
 
-
-//define your current location state
-//let currentLocation = Tardis;
-
-//Create a start game function to tell you what your current room is
-// ******the name property does not seem to be working, I had it working at one point but now only description works
-async function startGame(){
-  const actionMessage = `You are currently in the ${Tardis.description}.\n
-  In front of you is the control panel with an [up] and [down] control switch.\n
-  To the left of you are the doors to the outside.\n
-  What would you like to do?\n  >_`;
-  let answer = await ask(actionMessage);
- 
- //  //Logic to check to see if the user inputs Up or Down 
- //  while(true){
- //    if (answer === "Open Door"){
- //      console.log("You cannot open the door")
- //    }
- //    if(answer ==="Up Control Switch"){
- //     roomLookUp
- //    }
- //  }
- // }
- 
-
 //create lookup table to map the room keyword to the object
 let roomLookUp = {
   tardis: Tardis,
+  jungle: Jungle,
+  city: City,
   hallway: Hallway,
-  power: Power,
-  cell: Cell,
   labratory: Labratory,
   center: Center,
 };
 
 //create state machine to hold allowable transitions
 let roomStateMachine = {
-  tardis: ["Hallway"],
-  hallway: ["Labratory", "Center", "Power"],
-  power: ["Hallway", "Cell"],
-  labratory:["Hallway", "Center"],
-  cell: ["Hallway"],
+  tardis: ["Jungle"],
+  jungle: ["City",],
+  city: ["Hallway"],
+  hallway: ["Labratory"],
+  labratory:["Center", "Hallway"],
   center: ["Hallway"],
 };
+
+
+
+//define the current location state
+let currentLocation = "tardis";
+
+//Create a start game function for a starting point for the game. You can move out of the Tardis into the jungle
+async function startGame(){
+  const actionMessage = `You are currently in the ${roomLookUp[currentLocation].name} ${roomLookUp[currentLocation].description}.\n
+  In front of you is the control panel and it looks like you can [use] it.\n
+  To the left of you are the doors where the jungle lies outside. You could [move jungle] to explore the jungle.\n
+  What would you like to do?\n  >_`;
+  let answer = await ask(actionMessage);
+ 
+  if (answer === "use"){
+    roomLookUp[currentLocation].use()
+  }
+  if(answer === "move jungle"){
+    roomLookUp[currentLocation].move()
+    if(roomStateMachine[currentLocation].includes("Jungle")){
+      currentLocation = "jungle"
+      jungle(currentLocation);
+      //console.log(currentLocation);
+    }
+  }
+  else{
+    console.log("You cant go there from here");
+  }
+}
+
+
+
+/*I'm thinking maybe I don't need sperate async functions for each location, but maybe one async function
+to include all of the rooms and possible ways to move around. I'm still trying figure out how that
+would work.
+*/
+//Function to move to the jungle
+async function jungle(currentLocation){
+  const actionMessage = `You are currently in the ${roomLookUp[currentLocation].name}.\n
+  To the right of you is a village with people in it. It looks like you can [talk] to them.\n
+  Beyond the jungle is a city. It looks like you could [move] there.
+  What would you like to do?\n  >_`;
+  let answer = await ask(actionMessage);
+ 
+  if (answer === "talk"){
+    roomLookUp[currentLocation].talk()
+    //***This does not seem to work to update the location and move to the city. */
+    if(roomStateMachine[currentLocation].includes("City")){
+      //console.log(currentLocation)
+      currentLocation = "city"
+  }
+}
+  if(answer === "move city"){
+    roomLookUp[currentLocation].move()
+    if(roomStateMachine[currentLocation].includes("City")){
+      currentLocation = "city"
+      //console.log(currentLocation);
+    }
+  }
+  else{
+    console.log("You cant go there from here");
+  }
+  
+}
+
+//function to move to the city
+async function city(currentLocation){
+  const actionMessage = `You are currently in the ${roomLookUp[currentLocation].name}.\n
+  Infront of you are doors and a keypard\n
+  What would you like to do?\n  >_`;
+  let answer = await ask(actionMessage);
+ 
+  if (answer === "open door"){
+    roomLookUp[currentLocation].talk();
+  }
+  if(answer === "move city"){
+    roomLookUp[currentLocation].move()
+    if(roomStateMachine[currentLocation].includes("City")){
+      currentLocation = "city"
+      console.log(currentLocation);
+    }
+  }
+  else{
+    console.log("You cant go there from here");
+  }
 }
